2e94c566c7f0aac168b9ce4730dd655ee58f6f7a
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-28 12:21:20 +0000

    Add Reload Link.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/captcha_viewhelper/trunk@45787 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/CaptchaViewHelper.php
+++ Classes/ViewHelpers/CaptchaViewHelper.php
@@ -10,7 +10,13 @@ class Tx_CaptchaViewhelper_ViewHelpers_CaptchaViewHelper extends Tx_Fluid_ViewHe
 	 */
 	public function render() {
 		$path = t3lib_div::getIndpEnv('TYPO3_SITE_URL').'?eID=captcha_viewhelper_captcha&c='.time();
-		$image = '<img src="'.$path.'" class="captcha-image"/>';
+		$image = '<p>';
+			$image .= '<img id="captcha-image" src="'.$path.'" class="captcha-image"/>';
+			$image .= '<br />';
+			$image .= '<a class="captcha-reaload" href="javascript:void(0)" onclick="var now = new Date(); var e = document.getElementById(\'captcha-image\');e.src=e.src+\'&c=\'+now.getTime();">';
+				$image .= Tx_Extbase_Utility_Localization::translate('reload','captcha_viewhelper');
+			$image .= '</a>';
+		$image .= '</p>';
 		return $image;
 	}
 }
