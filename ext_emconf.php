90181e7aa90aadbe97c4b91638151080f326ccf7
axeljung01 <axeljung01@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-09 15:09:40 +0000

    Increase version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/captcha_viewhelper/trunk@50808 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "captcha_viewhelper".
 #
-# Auto generated 28-03-2011 14:20
+# Auto generated 09-08-2011 17:09
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -22,14 +22,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'beta',
+	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.1.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:27:{s:21:"ext_conf_template.txt";s:4:"9849";s:12:"ext_icon.gif";s:4:"f19a";s:17:"ext_localconf.php";s:4:"3948";s:19:"Classes/Captcha.php";s:4:"922b";s:49:"Classes/Validation/Validator/CaptchaValidator.php";s:4:"66ae";s:41:"Classes/ViewHelpers/CaptchaViewHelper.php";s:4:"a7ef";s:40:"Contrib/cool-php-captcha-0.3/LICENSE.txt";s:4:"d322";s:40:"Contrib/cool-php-captcha-0.3/captcha.php";s:4:"37f8";s:45:"Contrib/cool-php-captcha-0.3/example-form.php";s:4:"60ac";s:40:"Contrib/cool-php-captcha-0.3/example.jpg";s:4:"c43a";s:60:"Contrib/cool-php-captcha-0.3/resources/fonts/AntykwaBold.ttf";s:4:"b910";s:56:"Contrib/cool-php-captcha-0.3/resources/fonts/Candice.ttf";s:4:"9c7b";s:64:"Contrib/cool-php-captcha-0.3/resources/fonts/Ding-DongDaddyO.ttf";s:4:"7bc5";s:56:"Contrib/cool-php-captcha-0.3/resources/fonts/Duality.ttf";s:4:"0a28";s:57:"Contrib/cool-php-captcha-0.3/resources/fonts/Heineken.ttf";s:4:"fbe8";s:53:"Contrib/cool-php-captcha-0.3/resources/fonts/Jura.ttf";s:4:"33a1";s:57:"Contrib/cool-php-captcha-0.3/resources/fonts/StayPuft.ttf";s:4:"5929";s:66:"Contrib/cool-php-captcha-0.3/resources/fonts/TimesNewRomanBold.ttf";s:4:"ed6e";s:61:"Contrib/cool-php-captcha-0.3/resources/fonts/VeraSansBold.ttf";s:4:"4ebf";s:73:"Contrib/cool-php-captcha-0.3/resources/tools/dictionary-import/README.txt";s:4:"62ee";s:78:"Contrib/cool-php-captcha-0.3/resources/tools/dictionary-import/importwords.php";s:4:"cf03";s:82:"Contrib/cool-php-captcha-0.3/resources/tools/dictionary-import/sample-words-en.txt";s:4:"d750";s:82:"Contrib/cool-php-captcha-0.3/resources/tools/dictionary-import/sample-words-es.txt";s:4:"86e8";s:51:"Contrib/cool-php-captcha-0.3/resources/words/en.php";s:4:"ef0f";s:51:"Contrib/cool-php-captcha-0.3/resources/words/es.php";s:4:"6af4";s:40:"Resources/Private/Language/locallang.xml";s:4:"e696";s:33:"Resources/Private/PHP/captcha.php";s:4:"4c39";}',
+	'_md5_values_when_last_written' => 'a:27:{s:21:"ext_conf_template.txt";s:4:"9849";s:12:"ext_icon.gif";s:4:"f19a";s:17:"ext_localconf.php";s:4:"3948";s:19:"Classes/Captcha.php";s:4:"796f";s:49:"Classes/Validation/Validator/CaptchaValidator.php";s:4:"66ae";s:41:"Classes/ViewHelpers/CaptchaViewHelper.php";s:4:"a7ef";s:40:"Contrib/cool-php-captcha-0.3/LICENSE.txt";s:4:"d322";s:40:"Contrib/cool-php-captcha-0.3/captcha.php";s:4:"37f8";s:45:"Contrib/cool-php-captcha-0.3/example-form.php";s:4:"60ac";s:40:"Contrib/cool-php-captcha-0.3/example.jpg";s:4:"c43a";s:60:"Contrib/cool-php-captcha-0.3/resources/fonts/AntykwaBold.ttf";s:4:"b910";s:56:"Contrib/cool-php-captcha-0.3/resources/fonts/Candice.ttf";s:4:"9c7b";s:64:"Contrib/cool-php-captcha-0.3/resources/fonts/Ding-DongDaddyO.ttf";s:4:"7bc5";s:56:"Contrib/cool-php-captcha-0.3/resources/fonts/Duality.ttf";s:4:"0a28";s:57:"Contrib/cool-php-captcha-0.3/resources/fonts/Heineken.ttf";s:4:"fbe8";s:53:"Contrib/cool-php-captcha-0.3/resources/fonts/Jura.ttf";s:4:"33a1";s:57:"Contrib/cool-php-captcha-0.3/resources/fonts/StayPuft.ttf";s:4:"5929";s:66:"Contrib/cool-php-captcha-0.3/resources/fonts/TimesNewRomanBold.ttf";s:4:"ed6e";s:61:"Contrib/cool-php-captcha-0.3/resources/fonts/VeraSansBold.ttf";s:4:"4ebf";s:73:"Contrib/cool-php-captcha-0.3/resources/tools/dictionary-import/README.txt";s:4:"62ee";s:78:"Contrib/cool-php-captcha-0.3/resources/tools/dictionary-import/importwords.php";s:4:"cf03";s:82:"Contrib/cool-php-captcha-0.3/resources/tools/dictionary-import/sample-words-en.txt";s:4:"d750";s:82:"Contrib/cool-php-captcha-0.3/resources/tools/dictionary-import/sample-words-es.txt";s:4:"86e8";s:51:"Contrib/cool-php-captcha-0.3/resources/words/en.php";s:4:"ef0f";s:51:"Contrib/cool-php-captcha-0.3/resources/words/es.php";s:4:"6af4";s:40:"Resources/Private/Language/locallang.xml";s:4:"e696";s:33:"Resources/Private/PHP/captcha.php";s:4:"4c39";}',
 	'suggests' => array(
 	),
 );
